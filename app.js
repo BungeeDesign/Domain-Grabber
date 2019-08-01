1fe1e9f934057af10183c430b8d4293a19d77edd
BungeeDesign <james@bungeedesign.com>
2019-08-01 05:10:00 +0100

    Directory Offset & Firebase Admin image storage

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 // Domain Grabber - Custom sub-domain finder for Pentesting recon.
 // @author James Rogers
-
 'use strict';
 
 import dotenv from 'dotenv';
 import axios from 'axios';
-import fse from 'fs-extra';
+import fse, { existsSync } from 'fs-extra';
 import ora from 'ora';
 import jsdom from 'jsdom';
 const { JSDOM } = jsdom;
 import WebCapture from 'webpage-capture';
 import express from 'express';
+import admin from 'firebase-admin';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import path from 'path';
 
 // ENV Setup
 dotenv.config();
@@ -21,6 +22,18 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+// GCS Firebase Storage Init
+admin.initializeApp({
+    credential: admin.credential.cert('./domain-grabber-1b1c758f465e.json'),
+    storageBucket: 'domain-grabber.appspot.com'
+});
+
+const bucket = admin.storage().bucket();
+
+// Globals
+let screenshotURLs = [],
+dirSaved = '';
 
 // Root - Forbidden
 app.get('/', (req, res) => res.json({'status': 'Forbidden entry'}));
@@ -31,7 +44,6 @@ app.post('/domain', async (req, res) => {
     const data = await run(req.body[0]);
     res.send(data);
 });
-
 async function run(query) {
     const spinner = ora(`Finding sub-domains for - ${query.domain}`).start();
 
@@ -39,14 +51,21 @@ async function run(query) {
     const resp = await axios(`https://crt.sh/?q=%.${query.domain}`);
 
     // Create Directory for domain screenshots
-    try {   
-        await fse.mkdir(`./screenshots/${query.domain}`);
+    let dirOffset = Math.floor(Math.random()*9000) + 1000;
+    try {
+        if (existsSync(`./screenshots/${query.domain}`)) {
+            await fse.mkdir(`./screenshots/${query.domain}-${dirOffset}`);
+            dirSaved = `${query.domain}-${dirOffset}`;
+        } else {
+            await fse.mkdir(`./screenshots/${query.domain}`);
+            dirSaved = query.domain;
+        }   
     } catch (err) {
-        console.error(err);
+        console.log('Folder already exsists');
     }
 
     const capturer = new WebCapture({
-        outputDir: `./screenshots/${query.domain}`
+        outputDir: `./screenshots/${dirSaved}`
     });
     
     const dom = new JSDOM(resp.data);
@@ -54,15 +73,17 @@ async function run(query) {
 
     let crtSH = [];
     let crtshDomainsHTTPS = [];
+    let screenshotDomainArray = [];
 
     let i = 0;
     domains.forEach((domain) => {
-        crtSH.push({key: i++, domain: domain.innerHTML});
+        crtSH.push({key: i++, domain: domain.innerHTML, screenshot: ''});
     });
 
     let j = 0;
     crtSH.forEach((domain) => {
         crtshDomainsHTTPS.push({ key: j++, domain: `https://${domain.domain}`});
+        screenshotDomainArray.push(`https://${domain.domain}`);
     });
 
     spinner.stop();
@@ -75,15 +96,41 @@ async function run(query) {
             'domainName': query.domain,
             'noResults': crtSH.length == 0 ? true : false
         }
+        if (query.screenshot) {
+            spinner.start('Screenshotting Domains...');
+            const res = await capturer.capture(screenshotDomainArray);
+        
+            let paths = [];
+            for (const results of res) {
+                paths.push(results.output);
+            }
+
+            // Upload Screenshots
+            const uploadedScreenshots = uploadScreenshots(paths);
+
+            for (let i = 0; i < crtSH.length; i++) {
+                crtSH[i].screenshot = getScreenShots(i);
+            }
+            
+            function getScreenShots(i) {
+                if (i >= uploadedScreenshots.length) {
+                    return 'offline.png';
+                }
+                return uploadedScreenshots[i];
+            }
+
+            domainList = {
+                'domains': crtSH,
+                'domainName': query.domain,
+                'noResults': crtSH.length == 0 ? true : false
+            }
+        } else {
+            return domainList;
+        }
+
         return domainList;
     }
     
-    if (query.action === 'S') {
-        spinner.start('Screenshotting Domains...');
-        console.log(__dirname + '/images');
-        const res = await capturer.capture(crtshDomainsHTTPS);
-        console.log(res);
-    }
     
     if (query.probe === true) {
         spinner.start('Probing All Domains...');
@@ -102,6 +149,7 @@ async function run(query) {
 async function probeDomains(domains) {
     let probedDomains = [];
     let k = 0;
+
     for (const domain of domains) {
         try {            
             const res = await axios(domain.domain, { validateStatus: false }, { timeout: 2 });
@@ -113,8 +161,22 @@ async function probeDomains(domains) {
             console.log('Error');
         }
     };
+
     console.log('Finished....');
     return probedDomains;
+}
+
+function uploadScreenshots(paths) {
+    // console.log('UPLOAD SCREENSHOT', paths);
+    paths.forEach(async pathName => {
+        // Format Windows path 
+        let pathFormatted = path.resolve(pathName);
+        bucket.upload(pathFormatted);
+        const fileName = path.basename(pathFormatted);
+        screenshotURLs.push(`https://storage.googleapis.com/domain-grabber.appspot.com/${fileName}`);
+    });
+    // console.log('RETURN SCREENSHIT', screenshotURLs);
+    return screenshotURLs;
 }
 
 // Start App Listen on Port 5000 set in .env
